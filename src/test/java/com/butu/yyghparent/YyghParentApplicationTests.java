02996b86e3761b43de3b2f451034da198f0c887d
BUTUbird <1781894948@qq.com>
2021-08-16 20:42:46 +0800

    Initial commit

---
+++ src/test/java/com/butu/yyghparent/YyghParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.butu.yyghparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YyghParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
